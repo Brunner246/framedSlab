59423c9ac4380ba79f3d1e99f7400d773ce57ef8
Brunner246 <71121348+Brunner246@users.noreply.github.com>
2021-09-09 06:57:18 +0200

    Change Style

--- framedSlab.py
+++ framedSlab.py
@@ -1,20 +1,20 @@
 """framedSlab.py: create a simple framed slab in cadwork...."""
 
 # import modules
-import      utility_controller         as uc
-import      element_controller         as ec
 import      cadwork
-import      geometry_controller        as gc
 import      attribute_controller       as ac
+import      element_controller         as ec
+import      geometry_controller        as gc
+import      utility_controller         as uc
 import      visualization_controller   as vc
 
 
 WIDTH           = 60.           # beam width
 HEIGHT          = 240.          # beam height
 ORIGIN          = 0., 0., 0.    # start/origin point
-DIVISION        = 4             # number of divsion -> distribution (Sprungmass)
-BEAM_LENGTH     = 2800.         # length (Fuss-, Kopfholz)
-MEMBER_LENGTH   = 5000.         # length (Balken)
+DIVISION        = 4             # number of divsion -> distribution (beam distribution)
+BEAM_LENGTH     = 2800.         # length (beam)
+MEMBER_LENGTH   = 5000.         # length (member)
 
 
 def main():
@@ -27,37 +27,38 @@ def main():
     
     beam_length = BEAM_LENGTH # uc.get_user_double('Input beam length')
     
-    # Fussholz
-    beam_1 = createBeam(beam_length, 
+    # create start beam of frame
+    beam_1 = create_beam(beam_length, 
                         cadwork.point_3d(*ORIGIN), 
                         cadwork.point_3d(0., 1., 0.), 
                         cadwork.point_3d(0., 0., 1.), 
                         'beam')
     
     # get vectors (beam_1)
-    x_dir_beam, y_dir_beam, z_dir_beam = getBeamVectors(beam_1)
+    x_dir_beam, y_dir_beam, z_dir_beam = get_beam_vectors(beam_1)
     
     # move cadwork point -> start point for beam_2 (Kopfholz)
     start_point_beam_2 = cadwork.point_3d(*ORIGIN) + y_dir_beam * (MEMBER_LENGTH + WIDTH)
     
-    # Kopfholz
-    beam_2 = createBeam(beam_length, 
+    # create end beam of frame
+    beam_2 = create_beam(beam_length, 
                         start_point_beam_2, 
                         cadwork.point_3d(0., 1., 0.), 
                         cadwork.point_3d(0., 0., 1.), 
                         'beam')
     
     # move cadwork point -> start point for members
-    start_point_member = cadwork.point_3d(*ORIGIN) + y_dir_beam * .5 * WIDTH + cadwork.point_3d(*ORIGIN) + x_dir_beam * .5 * WIDTH
+    start_point_member = cadwork.point_3d(*ORIGIN) + y_dir_beam * .5 * WIDTH \
+                        + cadwork.point_3d(*ORIGIN) + x_dir_beam * .5 * WIDTH
     
     global DIVISION
-    # calculate division distance (Sprungmass)
-    spacing = beamSpacing(beam_1, DIVISION)
+    # calculate division distance
+    spacing = beam_spacing(beam_1, DIVISION)
     
     # while loop to create members -> increase spacing division
     n = 0.
     while n <= beam_length:
-        member = createBeam(MEMBER_LENGTH, 
+        member = create_beam(MEMBER_LENGTH, 
                             start_point_member + 
                             x_dir_beam * n, y_dir_beam, 
                             z_dir_beam)
@@ -67,7 +68,7 @@ def main():
 
 #---------------------------------------------------------------
 
-def createBeam(length, p1, x_dir, z_dir, name='member', color=3):
+def create_beam(length, p1, x_dir, z_dir, name='member', color=3):
     """Creates a rectangular member with attributes: name, color
 
     Parameters
@@ -77,9 +78,9 @@ def createBeam(length, p1, x_dir, z_dir, name='member', color=3):
     p1 : point_3d
         start point x, y, z
     x_dir : point_3d
-        local x-vector x, y, z -> cadwork.point_3d(0., 1., 0.)
+        local x-vector x, y, z e.g. -> cadwork.point_3d(0., 1., 0.)
     z_dir : point_3d
-        local z-vector x, y, z -> cadwork.point_3d(1., 0., 0.)
+        local z-vector x, y, z e.g. -> cadwork.point_3d(1., 0., 0.)
     name : str, optional
         set a name -> default = beam
 
@@ -90,14 +91,18 @@ def createBeam(length, p1, x_dir, z_dir, name='member', color=3):
     """
     global WIDTH
     global HEIGHT
-    beam = ec.create_rectangular_beam_vectors(WIDTH, HEIGHT, length, p1, x_dir, z_dir)
+    beam = ec.create_rectangular_beam_vectors(WIDTH, HEIGHT, 
+                                              length, 
+                                              p1, 
+                                              x_dir, 
+                                              z_dir)
     ac.set_name([beam], name)
     vc.set_color([beam], color)
     
     return beam
 
 
-def getBeamVectors(element_id):
+def get_beam_vectors(element_id):
     """Returns vectors (x,y,z) 
 
     Parameters
@@ -117,7 +122,7 @@ def getBeamVectors(element_id):
     return x_dir_sill, y_dir_sill, z_dir_sill
 
 
-def beamSpacing(beam, subdivisions):
+def beam_spacing(beam, subdivisions):
     """Calculates the beam/member spacing (Sprungmass)
 
     Parameters
